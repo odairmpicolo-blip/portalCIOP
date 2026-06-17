7abc08116c44c7e4578c6f040be5e1668285811c
odairmpicolo-blip <odairmpicolo@gmail.com>
2026-06-17 13:53:58 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig={
-apiKey:"AIzaSyDNMnb01cPrfJQkP-M44LE6bgwJMYq2Cq8",
-authDomain:"portal-ciop.firebaseapp.com",
-projectId:"portal-ciop",
-storageBucket:"portal-ciop.firebasestorage.app",
-messagingSenderId:"455189133437",
-appId:"1:455189133437:web:34dae49eb1fdaf65191914"
-};
