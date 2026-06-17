a9f09d4a8906ebf3a9d8d70e4b49804f5b6f790b
odairmpicolo-blip <odairmpicolo@gmail.com>
2026-06-17 15:27:44 -0300

    Update usuarios.js

--- assets/js/usuarios.js
+++ assets/js/usuarios.js
@@ -1,14 +1,22 @@
 export const usuarios = {
-  "admin@ciop.com.br": {
-    nome: "Administrador",
+  "odair.marin@icloud.com": {
+    nome: "Odair Marino",
     perfil: "Administrador"
   },
-  "supervisor@ciop.com.br": {
-    nome: "Supervisor",
+  "rehder@tcgl.com.br": {
+    nome: "Rehder",
     perfil: "Supervisor"
   },
-  "analista@ciop.com.br": {
-    nome: "Analista",
+  "monitoramento@tcgl.com.br": {
+    nome: "Islan Jesus",
+    perfil: "Supervisor"
+  },
+  "danielmartin@tcgl.com.br": {
+    nome: "Daniel Martins",
+    perfil: "Supervisor"
+  },
+  "ciopcontroladores@gmail.com": {
+    nome: "CIOP",
     perfil: "Analista"
   }
 };
