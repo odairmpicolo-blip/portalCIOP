da7991428280f71e2c3635579f12e13a10c10275
odairmpicolo-blip <odairmpicolo@gmail.com>
2026-06-17 22:24:07 -0300

    Update usuarios.js

--- assets/js/usuarios.js
+++ assets/js/usuarios.js
@@ -1,14 +1,3 @@
 export const usuarios = {
-  "admin@ciop.com.br": {
-    nome: "Administrador",
-    perfil: "Administrador"
-  },
-  "supervisor@ciop.com.br": {
-    nome: "Supervisor",
-    perfil: "Supervisor"
-  },
-  "analista@ciop.com.br": {
-    nome: "Analista",
-    perfil: "Analista"
-  }
+  
 };
